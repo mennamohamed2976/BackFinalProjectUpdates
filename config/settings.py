885a2789e65b84a2b7050ce095bd0b26f3c3f6a8
mennamohamed2976 <menna.8762004@gmail.com>
2026-06-16 20:29:31 +0300

    Update settings.py to fix cloudinary_storage entry
    Removed duplicate 'cloudinary_storage' entry and added 'MEDIA_URL' for media files.

--- config/settings.py
+++ config/settings.py
@@ -39,11 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
     'rest_framework.authtoken',
-    'cloudinary_storage',
     'cloudinary',
     'orgen',
 ]
@@ -176,7 +176,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL = '/media/'
 #AI MATCHING
 
 # AI_SERVICE_URL = "https://old-profusely-latticed.ngrok-free.app"
